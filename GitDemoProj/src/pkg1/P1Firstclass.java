13d2880984ce99b4c13c3006820195615e9558f0
Keerthi <Keerthi@DESKTOP-M2CO299.attlocal.net>
2019-01-10 23:05:30 -0600

    first class commit

--- GitDemoProj/src/pkg1/P1Firstclass.java
+++ GitDemoProj/src/pkg1/P1Firstclass.java
@@ -5,6 +5,10 @@ public class P1Firstclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/*
+		 pkg 1 first class
+		 * 
+		 */
 	}
 
 }
--- GitDemoProj/src/pkg2/P2Firstclass.java
+++ GitDemoProj/src/pkg2/P2Firstclass.java
@@ -3,8 +3,11 @@ package pkg2;
 public class P2Firstclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*
+		 *pkg2 second class
+		 * 
+		 */
 	}
 
 }

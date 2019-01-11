6b1693dbfe8b10cfd7eac9d7da3ef155c7d5dfaf
Keerthi <Keerthi@DESKTOP-M2CO299.attlocal.net>
2019-01-10 23:11:46 -0600

    first commit in firstbranch

--- GitDemoProj/src/pkg1/P1Secondclass.java
+++ GitDemoProj/src/pkg1/P1Secondclass.java
@@ -4,7 +4,11 @@ public class P1Secondclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*
+		 *pkg1 second class
+		 * 
+		 * 
+		 */
 	}
 
 }
--- GitDemoProj/src/pkg2/P2Secondclass.java
+++ GitDemoProj/src/pkg2/P2Secondclass.java
@@ -4,7 +4,11 @@ public class P2Secondclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*
+		 * 
+		 pkg2 second class
+		 * 
+		 */
 	}
 
 }
